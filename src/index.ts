ac91b143a9f32957824eddbb4fbdd836bfa81648
hrushikesh44 <hrushikeshmodupalli12345@gmail.com>
2025-02-07 02:24:37 +0530

    schema and middlewares

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,54 @@
 import express from  "express";
 import cors from "cors";
+import { UserModel } from "./db";
+import jwt from "jsonwebtoken";
+import { JWT_PASSWORD } from "./config";
+import { userMiddleware } from "./middleware";
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
+
+app.post('api/v1/signup', async (req, res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+
+    try{ 
+        await UserModel.create({
+            username: username,
+            password: password
+        })
+        res.status(200).json({
+            message: "Signed up successfully"
+        })
+    } catch(e){
+        res.status(411).json({
+            message: "User already exists"
+        })
+    }
+})
+
+app.post('api/v1/signin', async(req, res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+
+    const user = await UserModel.findOne({
+        username, 
+        password
+    })
+
+    if(user){
+        const token = jwt.sign({
+            id: user._id
+        }, JWT_PASSWORD)
+
+        res.json(token)
+    } else{
+        res.status(411).json({
+            message: "Invalid Credentials"
+        })
+    }
+})
 
 app.listen(3000);
