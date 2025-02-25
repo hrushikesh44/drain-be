18071bc0456a0cf4adc203e9ca5b628edb608eab
hrushikesh44 <hrushikeshmodupalli12345@gmail.com>
2025-02-25 23:06:46 +0530

    ep

--- src/db.ts
+++ src/db.ts
@@ -2,7 +2,7 @@ import mongoose, { Schema, model } from "mongoose";
 
 const url: string = process.env.MONGODB_URI ;
 
-mongoose.connect("mongodb+srv://hrushikesh44:dCkf2VMypSsBKha@cluster0.2lasb.mongodb.net/drain");
+mongoose.connect(url);
 
 const UserSchema = new Schema({
     username: {type: String, unique: true},
